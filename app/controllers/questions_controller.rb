3b857f05239e9b1d6db9ca65e83fc40c03dd611f
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-09-28 13:57:19 -0400

    getting questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,41 @@
 class QuestionsController < ApplicationController
+
+    def getquestions
+        question = ''
+        finalArr = [] 
+        questions = Question.all.sample(25)
+
+        questions.each do |q|
+            choices = []
+            correct = ''
+            incorrect = []
+            
+            # byebug
+            
+
+            choices = Choice.where(question_id: q.id)
+            # byebug
+            choices.each do |c|
+                if c.iscorrect === true
+                    correct = c.choice_text
+                elsif c.iscorrect === false
+                    incorrect.push(c.choice_text)
+                end
+            end
+            
+            
+
+            finalArr.push(question = [ 
+                'question' => q.question, 
+                'choices' => {
+                    'correct' => correct,
+                    'incorrect' => incorrect
+                }
+            ])
+            
+            
+        end
+        
+        render json: {question: finalArr} 
+    end
 end
