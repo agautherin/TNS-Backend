69e929e0eda18b56e89a09220e22f6f0bc413bbf
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-10-11 18:57:33 -0400

    upddating routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   resources :choices
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/quiz", to: "questions#getquestions" 
+  get "/quiz", to: "questions#getquestions"
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      post '/login', to: 'auth#create'
+      get '/token', to: 'auth#token_log_in'
+    end
+  end
 end
